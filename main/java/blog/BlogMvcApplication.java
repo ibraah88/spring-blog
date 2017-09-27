635b040eb8e9fec104ffb65970e31694deaa96b4
isdiop <info@isdiop.com>
2017-09-27 18:22:04 +0200

    first commit

---
+++ main/java/blog/BlogMvcApplication.java
@@ -0,0 +1,15 @@
+package blog;
+
+/**
+ * Created by ibraa on 27/09/2017.
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogMvcApplication {
+    public static void main(String[] args){
+        SpringApplication.run(BlogMvcApplication.class, args);
+    }
+
+}
---
+++ main/java/blog/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package blog.controllers;
+/**
+ * Created by ibraa on 27/09/2017.
+ */
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+        public String index(){
+
+        return "index";
+    }
+
+}
